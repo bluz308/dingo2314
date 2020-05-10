b632737792930711f6d6f1c522d01dfea2dcbacd
Van Duc <vanduc2314@gmail.com>
2020-05-10 14:06:33 +0700

    update firebase

--- Admin/firebase.js
+++ Admin/firebase.js
@@ -5,12 +5,12 @@ import "firebase/auth";
 import "firebase/database";
 // Your web app's Firebase configuration
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyAnfD9rwyDfT7FToySvYxukautWWJ-Y36k",
+  authDomain: "dingo-2314.firebaseapp.com",
+  databaseURL: "https://dingo-2314.firebaseio.com",
+  projectId: "dingo-2314",
+  storageBucket: "dingo-2314.appspot.com",
+  messagingSenderId: "200666974505",
 };
 
 if (!firebase.apps.length) {
